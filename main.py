6bf688d30b5981aa5cc2dd0e8f3097f2a418e1af
Himanshu Ranjan <84950556+RanjanH@users.noreply.github.com>
2024-02-15 00:33:57 +0530

    added speech to text

---
+++ main.py
@@ -0,0 +1,31 @@
+import speech_recognition as sr
+import pyttsx3
+import nltk
+
+r = sr.Recognizer()
+semantics = {'switch' : ['come back','switch','go','take care'],'move' : ['use']}
+
+with sr.Microphone() as source:
+    try:
+        r.adjust_for_ambient_noise(source, duration=1)
+        r.pause_threshold = 0.5
+        print("Started Listening.....")
+        while True:
+            audio = r.listen(source)
+            print(f"Recognizing the audio")
+            try:
+                cmd = r.recognize_google(audio,language = "en-in")
+                print(f"Did you say : {cmd}")
+                token = nltk.word_tokenize(cmd)
+                tagged = nltk.pos_tag(token)
+                print(tagged)
+                #gram2 = [token[i] + ' ' + token[i+1] for i in range(0,len(token))]
+
+            except Exception as e:
+                print(f"Come again....{e}")
+
+    except sr.RequestError as e:
+        print(f"Could not request results : {e}")
+
+    except sr.UnknownValueError:
+        print("Unknown Value Error")
