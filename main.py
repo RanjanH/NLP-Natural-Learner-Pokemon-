8ac6133f163b8acfde4eafed6177b801dc38ef63
Himanshu Ranjan <84950556+RanjanH@users.noreply.github.com>
2024-06-07 21:41:14 +0530

    added named entity identifier adn command identifier

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 import json
 
 r = sr.Recognizer()
-semantics = {'switch' : ['come back','switch','go','take care'],'move' : ['use']}
+semantics = {'switch' : ['come back go','switch with'],'move' : ['use','strike with','attack with']}
 cmds = ['switch','come back','use','go']
 
 pokeData = open("data/species.json",'r')
@@ -16,8 +16,10 @@ moves = json.load(moveData)
 def pos_tag(tokens):
     tagged = []
     for i in range(0,len(tokens)):
-        if tokens[i].lower() in pokemons or tokens[i].lower() in moves:
+        if tokens[i].lower() in pokemons:
             tagged.append((tokens[i],'NNP'))
+        elif tokens[i].lower() in moves:
+            tagged.append((tokens[i],'NNM'))
         elif tokens[i] in cmds:
             if i != 0:
                 if tokens[i-1].lower() in pokemons or tokens[i+1].lower() in pokemons:
@@ -31,10 +33,46 @@ def pos_tag(tokens):
                 tagged.append(i)
     return tagged
 
-def grams(text):
-    gram = {1:[],2:[]}
-    for i in text:
-        pass
+def namedEntityExtractor(tagged):
+    pokemon = {1:'',2:''}
+    move = ''
+    for (name,tag) in tagged:
+        if tag == 'NNP':
+            if not pokemon[1]:
+                pokemon[1] = name
+            else:
+                pokemon[2] = name
+        elif tag == 'NNM':
+            move = name
+
+    return pokemon, move
+
+def commandIdentifier(tagged):
+    global semantics
+    gram = {1:[],2:''}
+    cmd = ''
+    for (name,tag) in tagged:
+        if tag != 'NNP' and tag != 'NNM':
+            gram[1].append(name)
+    if len(gram[1]) == 2:
+        gram[2] += gram[1][0] + ' ' + gram[1][1]
+        cmd = gram[2]
+    elif len(gram[1]) == 3:
+        for i in gram[1]:
+            gram[2] += i + ' '
+        gram[2] = gram[2].strip()
+        cmd = gram[2]
+    elif len(gram[1]) != 0:
+        cmd = gram[1][0]
+    flag = False
+    for i in semantics.keys():
+        if cmd in semantics[i]:
+            flag = True
+            print(f"Command - {i}")
+            break
+    if not flag:
+        print('Not a valid command')        
+
 
 with sr.Microphone() as source:
     try:
@@ -53,6 +91,9 @@ with sr.Microphone() as source:
                 token = nltk.word_tokenize(cmd)
                 tagged = pos_tag(token)
                 print(tagged)
+                pokemon, move = namedEntityExtractor(tagged)
+                print(f"Pokemon - {pokemon}\nMove - {move}")
+                commandIdentifier(tagged)
                 #gram2 = [token[i] + ' ' + token[i+1] for i in range(0,len(token))]
 
             except Exception as e:
